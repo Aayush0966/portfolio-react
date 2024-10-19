f53fd5e9587a58c520477620ec99911763fe7f4b
Aayush0966 <dev@aayushbudhathoki.com.np>
2024-10-20 05:35:09 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
